58468e9bb98660af7338eefe20a88405c155f962
Akane0531 <53241815+Akane0531@users.noreply.github.com>
2024-05-19 00:42:03 +0900

    型定義がイマイチ分かってない

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+export async function GET() {
   const posts = await getCollection('posts');
 
   return rss({
